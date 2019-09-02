96055d23c99b8714e03c0d816f36703d19265354
lionelsenyana <lionelsenyana@gmail.com>
2019-09-01 20:46:29 -0600

    ft(api): adds endpoint and test for accept session
    -adds endpoints for accepting a session
    -adds test for accepting a session
    
    [Finishes #168130999]

--- server/tests/session-routes-test.js
+++ server/tests/session-routes-test.js
@@ -38,3 +38,32 @@ describe('User should create sessions', () => {
       });
   });
 });
+
+/*
+ * 2) Tests for accepting a session:
+ */
+describe('Mentor should accept a session', () => {
+    it('Expect to accept a session', (done) => {
+      chai.request(server)
+        .patch('/api/v1/sessions/' + global.savedUser.sessionId + '/accept')
+        .set('token', global.savedUser.token)
+        .send(mentor)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          done();
+        });
+    });
+    it('Expect to fail to accept a session', (done) => {
+      chai.request(server)
+      .patch('/api/v1/sessions/' + '12345' + '/accept')
+        .send(mentor)
+        .end((err, res) => {
+          console.log(res.body);
+          
+          expect(res).to.have.status(401);
+          expect(res.body).to.be.an('object');
+          done();
+        });
+    });
+  });
