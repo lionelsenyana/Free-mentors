0e5ff172a5e46fad66009935d938abdb06ae0102
lionelsenyana <lionelsenyana@gmail.com>
2019-09-01 19:25:40 -0600

    ft(api): adds endpoints and test for get mentors
    - adds endpoints for getting mentors
    -adds test for getting mentors
    
    [Finishes #168130827]

---
+++ server/tests/mentor-routes-test.js
@@ -0,0 +1,40 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../app';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const mentor = {
+  email: '1@gmail.com',
+  mentorId: '1234'
+};
+
+/*
+ * 1) Tests for getting a list of mentors:
+ */
+describe('User should get all mentors', () => {
+  it('Expect to get all mentors', (done) => {
+    chai.request(server)
+      .get('/api/v1/mentor/mentors')
+      .set('token',global.savedUser.token)
+      .send()
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+  it('Expect to fail to get all mentors', (done) => {
+    chai.request(server)
+      .get('/api/v1/mentor/mentors')
+      .send()
+      .end((err, res) => {
+        console.log(res.body);
+        
+        expect(res).to.have.status(401);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+});
