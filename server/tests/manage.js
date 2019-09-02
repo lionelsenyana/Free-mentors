078ca710adeb691bf0cc9f327421063018b846f3
lionelsenyana <lionelsenyana@gmail.com>
2019-09-01 20:25:01 -0600

    ft(api): add endpoints and test for create session
    -adds endpoints for creating a session
    -adds test for creating a session
    
    [Finishes #168131280]

--- server/tests/manage.js
+++ server/tests/manage.js
@@ -1,8 +1,10 @@
 import authRoutesTest from './auth-routes-test';
 import userRoutesTest from './user-routes-test';
 import mentorRoutesTest from './mentor-routes-test';
+import sessionTest from './session-routes-test';
 export {
     authRoutesTest,
     userRoutesTest,
     mentorRoutesTest,
+    sessionTest,
   };
--- server/tests/session-routes-test.js
+++ server/tests/session-routes-test.js
@@ -1,1 +1,40 @@
 
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../app';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const mentor = {
+  mentorId: '1234',
+};
+
+/*
+ * 1) Tests for creating a session:
+ */
+describe('User should create sessions', () => {
+  it('Expect to create sessions', (done) => {
+    chai.request(server)
+      .post('/api/v1/sessions')
+      .set('token', global.savedUser.token)
+      .send(mentor)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+  it('Expect to fail to create sessions', (done) => {
+    chai.request(server)
+      .post('/api/v1/sessions')
+      .send(mentor)
+      .end((err, res) => {
+        console.log(res.body);
+        
+        expect(res).to.have.status(401);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+});
